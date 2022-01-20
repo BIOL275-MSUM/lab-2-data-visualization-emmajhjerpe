c761b76a8bc4253a97afce4e0e51396d560fb9f5
emmajhjerpe <97974816+emmajhjerpe@users.noreply.github.com>
2022-01-20 10:55:59 -0600

    Delete assignment2.R

--- assignment2.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-library(palmerpenguins)
-penguins
-ggplot(data = penguins) + 
-  geom_point(mapping = aes(x = body_mass_g, y = flipper_length_mm, color = species, shape = island)) +
-  facet_wrap(~ species, nrow = 1) + 
-  facet_grid(species ~ island)
